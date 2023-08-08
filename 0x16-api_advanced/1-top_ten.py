03563da4aec773d3e3473dfb5907248f069410af
MosehMaguah <Mosehmaguah@gmail.com>
2023-08-08 17:03:31 +0300

    API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'limit': 10}
     response = req.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+                       params=parameters)
 
     if response.status_code == 200:
         titles_ = response.json().get('data').get('children')
